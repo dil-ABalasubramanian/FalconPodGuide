4a48dbcff87d73eedfb5600aa01e549797e95676
Abilash Balasubramanian <abalasubramanian@diligent.com>
2024-06-24 15:46:51 +0530

    Version from home swift

--- FalconPodGuide.podspec
+++ FalconPodGuide.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'FalconPodGuide'
-  s.version          = '0.1.1'
+  s.version          = '0.1.2'
   s.summary          = 'A short description of FalconPodGuide.'
 
 # This description is used to generate tags and improve search results.
--- FalconPodGuide/Classes/Home.swift
+++ FalconPodGuide/Classes/Home.swift
@@ -7,4 +7,10 @@ public class Home {
     public func canShowHomeScreen() -> Bool{
         return canShowHome
     }
+
+    public func printMyVersion() -> String {
+        guard let version = Bundle.main
+            .infoDictionary?["CFBundleShortVersionString"] as? String else { return "Unknown" }
+        return version
+    }
 }
