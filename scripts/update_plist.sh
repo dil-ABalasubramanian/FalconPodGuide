2ee37323d016265b602a74a808d9de2974c65ee2
Abilash Balasubramanian <abalasubramanian@diligent.com>
2024-12-09 11:46:18 +0530

    Updating the path - testing to run the successful build

--- scripts/update_plist.sh
+++ scripts/update_plist.sh
@@ -4,7 +4,8 @@
 set -e
 
 # Path to Info.plist
-INFO_PLIST="../Example/FalconPodGuide/Info.plist"
+INFO_PLIST=$(find ../Example/FalconPodGuide -name "Info.plist" | head -n 1)
+echo "path = $INFO_PLIST"
 
 # Get current version and build
 CURRENT_VERSION=$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" "$INFO_PLIST" 2>/dev/null || echo "0.0.0")
