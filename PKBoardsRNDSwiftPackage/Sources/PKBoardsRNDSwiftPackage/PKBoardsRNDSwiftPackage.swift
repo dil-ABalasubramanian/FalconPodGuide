979c36e321b78a767c50f41003c8f0b3a88b7392
Abilash Balasubramanian <abalasubramanian@diligent.com>
2025-05-07 11:12:34 +0530

    Docs added

--- PKBoardsRNDSwiftPackage/Sources/PKBoardsRNDSwiftPackage/PKBoardsRNDSwiftPackage.swift
+++ PKBoardsRNDSwiftPackage/Sources/PKBoardsRNDSwiftPackage/PKBoardsRNDSwiftPackage.swift
@@ -1,2 +1,22 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import SwiftUI
+
+public class PKBoardsRNDSwift {
+    private var info: String
+
+    public init(defaultInfo: String = "default-value") {
+        self.info = defaultInfo
+    }
+
+    /// Updates the stored info string
+    public func updateInfo(to newInfo: String) {
+        self.info = newInfo
+    }
+
+    /// Retrieves the current info string
+    public func getInfo() -> String {
+        return info
+    }
+}
